0d882ec1a0624612867246a3e0b357367f6f50c6
oxanaberne <oxana.berne@polymtl.ca>
2020-09-16 04:32:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     
-    old_char = string[6]
+    old_char = "w"
     new_char = 
     return
 
